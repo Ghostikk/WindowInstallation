1871c6f14245c96a883c6c9d10c265045bcec5ca
Vladimir Seledkin <vladimyr_seledkin@mail.ru>
2022-07-17 19:56:28 +0300

    init: Добавлена анимация в верхний блок с табамии. Добавлены анимации к 'Примерам работ'. Проведен рефакторинг кода. Внесены изменения в структуру HTML

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ import forms from './modules/forms';
 
 window.addEventListener("DOMContentLoaded", () => {
     modals();
-    tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active', 'fadeInLeft');
+    tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active', 'fadeInLeft', '.glazing_block a');
     tabs('.decoration_slider','.no_click', '.decoration_content > div > div', 'after_click');
     forms();
    
